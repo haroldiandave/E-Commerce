ce2fe61bd279987627b237aa31483a4c3cede778
haroldiandave <157860683+haroldiandave@users.noreply.github.com>
2024-04-01 19:33:06 +0800

    Updated Registration Form (#3)
    Updated Registration Form

---
+++ ecommerce-registration-form/Welcome.php
@@ -0,0 +1,18 @@
+
+<html>
+   <head>
+      <title></title>
+      <link rel="stylesheet" href="welcome.css">
+   </head>
+   <body>
+      <div class="background-container">
+         <div class="container">
+              <div class="box form-box">
+                  <h1>Welcome RCians!</h1> 
+                  <h2><a href = "logout.php">Sign Out</a></h2>
+               </div>
+         </div>   
+
+      </div>   
+   </body>
+</html>
